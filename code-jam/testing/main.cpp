f45d6c74b3bc413bdb4847e142c500ad31b8771d
Gustavo Gardusi <gardusi@pop-os.localdomain>
2021-04-02 18:12:04 -0300

    add test folder

---
+++ code-jam/testing/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+void solve_test_case(const int test) {
+  std::cout << "Case #" << test << ":" << std::endl;
+}
+
+int main() {
+  int T;
+  std::cin >> T;
+  for (int test = 1; test <= T; test += 1) {
+    solve_test_case(test);
+  }
+  return 0;
+}
