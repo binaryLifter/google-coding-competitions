895826c3cf9e67da80b3eaa1f3fa8a0d68a3e7e0
Gardusi <gardusig@38f9d3d02299.ant.amazon.com>
2021-04-11 16:35:46 -0300

    add fast io

--- code-jam/testing/main.cpp
+++ code-jam/testing/main.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 
 void solve_test_case(const int test) {
-  std::cout << "Case #" << test << ":" << std::endl;
+  std::cout << "Case #" << test << ":" << '\n';
 }
 
 int main() {
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.tie(0);
   int T;
   std::cin >> T;
   for (int test = 1; test <= T; test += 1) {
